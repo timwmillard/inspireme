b2cc2e975bad58b3d7a3a8eccdf8d37002d89bb5
Tim Millard <timwmillard@gmail.com>
2021-01-08 16:42:59 +1100

    Refactor main api server to handle different storage options

--- cmd/inspireme-api/main.go
+++ cmd/inspireme-api/main.go
@@ -12,11 +12,16 @@ import (
 	"github.com/timwmillard/inspireme/storage"
 )
 
+var (
+	mux              = http.NewServeMux()
+	logger           = log.New(os.Stdout, "inspireme ", log.LstdFlags)
+	bindAddress      = os.Getenv("BIND_ADDRESS")
+	inspireMeStorage = os.Getenv("INSPIREME_STORAGE")
+)
+
 func main() {
-	logger := log.New(os.Stdout, "inspireme ", log.LstdFlags)
 
 	// Bind Address
-	bindAddress := os.Getenv("BIND_ADDRESS")
 	if bindAddress == "" {
 		bindAddress = ":8080"
 	}
@@ -27,38 +32,12 @@ func main() {
 		fontsDir = "../../resources/fonts"
 	}
 
-	// File Storage
-	imagesStoragePath := os.Getenv("IMAGES_STORAGE_PATH")
-	if imagesStoragePath == "" {
-		imagesStoragePath = "images"
-	}
-	// Create directory if not exist
-	err := os.MkdirAll(imagesStoragePath, os.ModePerm)
-	if err != nil {
-		logger.Printf("unable to create image storage path: %s", err)
-		os.Exit(1)
-	}
-
-	// File Storage
-	imagesBaseURL := os.Getenv("IMAGES_BASE_URL")
-	if imagesBaseURL == "" {
-		imagesBaseURL = "http://localhost" + bindAddress + "/images/"
-	}
-
-	webFileStorage := &storage.WebLocalFile{
-		LocalDir:      imagesStoragePath,
-		ImagesBaseURL: imagesBaseURL,
-	}
-
 	// InspireMe Image Generator
 	imgGen := &inspireme.ImageGenerator{
-		Storage:  webFileStorage,
+		Storage:  imageStorage(),
 		Client:   http.DefaultClient,
 		FontsDir: fontsDir,
 	}
-
-	// Create mux handler
-	mux := http.NewServeMux()
 
 	// InspireMe HTTP Hander
 	inspiremeHandler := &inspireme.Handler{
@@ -66,9 +45,6 @@ func main() {
 		InspireMe: imgGen,
 	}
 	mux.Handle("/", inspiremeHandler)
-
-	imageServer := http.FileServer(http.Dir(imagesStoragePath))
-	mux.Handle("/images/", http.StripPrefix("/images/", imageServer))
 
 	// HTTP Server
 	server := http.Server{
@@ -103,3 +79,60 @@ func main() {
 	server.Shutdown(ctx)
 
 }
+
+func imageStorage() inspireme.Storage {
+
+	switch inspireMeStorage {
+	case "local":
+		return webLocalStorage()
+	// case "gcloud":
+	// 	return gCloudStorage()
+	// case "s3":
+	// 	return s3Storage()
+	default:
+		return webLocalStorage()
+	}
+
+}
+
+func webLocalStorage() *storage.WebLocalFile {
+
+	// File Storage
+	imagesStoragePath := os.Getenv("IMAGES_STORAGE_PATH")
+	if imagesStoragePath == "" {
+		imagesStoragePath = "images"
+	}
+	// Create directory if not exist
+	err := os.MkdirAll(imagesStoragePath, os.ModePerm)
+	if err != nil {
+		logger.Printf("unable to create image storage path: %s", err)
+		os.Exit(1)
+	}
+
+	// File Storage
+	imagesBaseURL := os.Getenv("IMAGES_BASE_URL")
+	if imagesBaseURL == "" {
+		imagesBaseURL = "http://localhost" + bindAddress + "/images/"
+	}
+
+	webFileStorage := &storage.WebLocalFile{
+		LocalDir:      imagesStoragePath,
+		ImagesBaseURL: imagesBaseURL,
+	}
+
+	// Setup Local image server
+	imageServer := http.FileServer(http.Dir(imagesStoragePath))
+	mux.Handle("/images/", http.StripPrefix("/images/", imageServer))
+
+	return webFileStorage
+}
+
+// func gCloudStorage() *storage.GCloud {
+
+// 	return nil
+// }
+
+// func s3Storage() *storage.S3 {
+
+// 	return nil
+// }
